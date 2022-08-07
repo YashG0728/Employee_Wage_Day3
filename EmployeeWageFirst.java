122b09da56fba3fa370713d95daa9b58eecbb05c
YashG0728 <yashgharde07@gmail.com>
2022-08-07 23:52:50 +0530

    Added:Employee_Attendance

---
+++ EmployeeWageFirst.java
@@ -0,0 +1,16 @@
+public class EmployeeWageFirst {
+    public static void main(String[] args) {
+
+        //CONSTANTS
+        int IS_PRESENT = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+
+        if (empCheck == IS_PRESENT) {
+            System.out.println("Employee is present");
+        } else {
+            System.out.println("Employee is absent");
+        }
+
+    }
+
+}
