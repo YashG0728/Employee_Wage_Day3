1542645d3a1456f64d20f059e332f7c92e797a07
YashG0728 <yashgharde07@gmail.com>
2022-08-08 00:04:06 +0530

    Added:Daily_Employee_Wage

--- EmployeeDailyWageTwo.java
+++ EmployeeDailyWageTwo.java
@@ -1,12 +1,16 @@
-public class EmployeeWageFirst {
+public class EmployeeDailyWageTwo {
     public static void main(String[] args) {
 
         //CONSTANTS
         int IS_PRESENT = 1;
+        int employeeHours = 8;
+        int wage_Per_Hr = 20;
         double empCheck = Math.floor(Math.random() * 10) % 2;
 
         if (empCheck == IS_PRESENT) {
             System.out.println("Employee is present");
+            int empWage = employeeHours * wage_Per_Hr;
+            System.out.println("Employee wage : "+empWage);
         } else {
             System.out.println("Employee is absent");
         }
@@ -14,3 +18,5 @@ public class EmployeeWageFirst {
     }
 
 }
+
+
